7b44e3ebffe46f163579ee697622478307168d76
sadia-iqbal-code <si9230558@gmail.com>
2025-01-30 18:44:37 +0500

    new code

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,9 @@
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'package:untitled1/SplashScreen.dart';
-
-
-
-
-
-
 import 'package:untitled1/userProvider.dart';
-
 import 'firebase_options.dart';
 
 
